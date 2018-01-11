e178fc3acd9e2de0e586730772a29f3017288d9b
Andy Evans <andrewevans0@outlook.com>
2018-01-11 20:22:47 +0000

    added changes

---
+++ lights.py
@@ -0,0 +1,19 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+sense.clear()
+sense.set_rotation(90)
+
+while True:
+    o = sense.get_orientation()
+    pitch = o["pitch"]
+    roll = o["roll"]
+    yaw = o["yaw"]
+    print("pitch {0} roll {1} yaw {2}".format(pitch, roll, yaw))
+    if pitch > 300:
+        sense.set_pixel(2, 2, (0, 0, 255))
+        sense.set_pixel(4, 2, (0, 0, 255))
+        sense.set_pixel(3, 4, (100, 0, 0))
+        sense.set_pixel(1, 5, (255, 0, 0))
+        sense.set_pixel(2, 6, (255, 0, 0))
+        sense.set_pixel(3, 6, (255, 0, 0))
+        sense.set_pixel(4, 6, (255, 0, 0))
