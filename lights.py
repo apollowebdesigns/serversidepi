8cc8f3164842b34703f9ace97df1178341177ed9
Andy Evans <andrewevans0@outlook.com>
2018-01-13 12:56:23 +0000

    test

--- lights.py
+++ lights.py
@@ -8,8 +8,13 @@ while True:
     roll = o["roll"]
     yaw = o["yaw"]
     print("pitch {0} roll {1} yaw {2}".format(pitch, roll, yaw))
+    count = 0
     if pitch > 300:
-        sense.set_pixel(2, 2, (0, 0, 255))
+        if count % 2 == 0:
+            sense.set_pixel(2, 2, (0, 0, 0))
+        else:
+            sense.set_pixel(2, 2, (0, 0, 255))
+        count++
         sense.set_pixel(4, 2, (0, 0, 255))
         sense.set_pixel(3, 4, (100, 0, 0))
         sense.set_pixel(1, 5, (255, 0, 0))
