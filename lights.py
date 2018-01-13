c2abc92debdf98b01aae5645e14768ea95974fac
Andy Evans <andrewevans0@outlook.com>
2018-01-13 12:59:07 +0000

    removed twitch

--- lights.py
+++ lights.py
@@ -10,11 +10,7 @@ while True:
     yaw = o["yaw"]
     print("pitch {0} roll {1} yaw {2}".format(pitch, roll, yaw))
     if pitch > 300:
-        if count % 2 == 0:
-            sense.set_pixel(2, 2, (0, 0, 0))
-        else:
-            sense.set_pixel(2, 2, (0, 0, 255))
-        count += 1
+        sense.set_pixel(2, 2, (0, 0, 255))
         sense.set_pixel(4, 2, (0, 0, 255))
         sense.set_pixel(3, 4, (100, 0, 0))
         sense.set_pixel(1, 5, (255, 0, 0))
