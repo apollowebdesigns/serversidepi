ecafa65505e9adadfa4bdc0bf22c61bf696c2277
Andy Evans <andrewevans0@outlook.com>
2018-01-13 12:57:00 +0000

    boff

--- lights.py
+++ lights.py
@@ -14,7 +14,7 @@ while True:
             sense.set_pixel(2, 2, (0, 0, 0))
         else:
             sense.set_pixel(2, 2, (0, 0, 255))
-        count++
+        count += 1
         sense.set_pixel(4, 2, (0, 0, 255))
         sense.set_pixel(3, 4, (100, 0, 0))
         sense.set_pixel(1, 5, (255, 0, 0))
