fa15995a57a28ac4ee3937dfc7eaadb6310e32be
Andy Evans <andrewevans0@outlook.com>
2018-03-23 18:32:34 +0000

    test

--- ultrasonic.py
+++ ultrasonic.py
@@ -31,7 +31,7 @@ while True:
     arduino.digitalWrite(TrigPin, arduino.HIGH)
     sleep(0.1)
     arduino.digitalWrite(TrigPin, arduino.LOW)
-    duration = arduino.digitalRead(EchoPin);
+    duration = arduino.analogueRead(EchoPin);
     distance = (duration*0.0343)/2;
     print('testing the loop')
     print(duration)
