f9678d80b37294b3da11d9b5c46453fb52d89f21
Andy Evans <andrewevans0@outlook.com>
2018-03-30 17:56:44 +0100

    checnnhwioehrfw

--- testdatabasepsql.py
+++ testdatabasepsql.py
@@ -1,11 +1,34 @@
 import psycopg2
+from configparser import ConfigParser
 import sys
 
 
 conn = None
 
+def config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+ 
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+ 
+    return db
+
 try:  
-    conn = psycopg2.connect(host="localhost",database="weather", user="postgres", password="postgres")
+    # read connection parameters
+    params = config()
+
+    # connect to the PostgreSQL server
+    print('Connecting to the PostgreSQL database...')
+    conn = psycopg2.connect(**params)
     cur = conn.cursor()
     cur.execute('SELECT version()')          
     ver = cur.fetchone()
