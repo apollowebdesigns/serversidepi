c28c74d4cb2896d1c87f0ac637fe8a94e7c4db61
Andy Evans <andrewevans0@outlook.com>
2018-03-30 17:51:18 +0100

    typo

--- testdatabasepsql.py
+++ testdatabasepsql.py
@@ -4,9 +4,8 @@ import sys
 
 con = None
 
-try:
-     
-    conn = psycopg2.connect("dbname=weather user=postgres password=postgres")
+try:  
+    con = psycopg2.connect("dbname=weather user=postgres password=postgres")
     cur = con.cursor()
     cur.execute('SELECT version()')          
     ver = cur.fetchone()
