ea32d1b37fbf36301cfece03bd4cdbb7cf2b9d93
Andy Evans <andrewevans0@outlook.com>
2018-04-16 20:32:00 +0100

    small change

--- static/app.js
+++ static/app.js
@@ -53,10 +53,10 @@ function engageAutomaticMode() {
                 console.log('right!');
                 $('#output').append('<li>'+message.data+'</li>');
             }
-            await sleep(500);
+            // await sleep(500);
             sseRight.close();
             await $.get( "/end_motor_source", function(data) {
-                console.log('ending');
+                console.log('right has finished');
                 $( ".result" ).html( data );
             });
         }
