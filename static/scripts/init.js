f1f6f0805bc57b26154c793e73077725d0588840
apollowebdesigns <andrewevans0@outlook.com>
2018-11-11 15:24:33 +0000

    changed server

--- static/scripts/init.js
+++ static/scripts/init.js
@@ -6,7 +6,7 @@
         //remove extra url for ui router params
         var removeSlash = currentUrl.length - 1;
         var secondUrl = currentUrl.toString().slice(0, removeSlash);
-        var rawness = String.raw`:2222/html/min.php`;
+        var rawness = String.raw`:8000`;
         var calcUrl = secondUrl.concat(rawness);
         console.log("the current url is");
         console.log(calcUrl);
