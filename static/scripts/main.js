b87f1429517109d445437c88e5a3fb34bc8a5ed1
Andy Evans <andrewevans0@outlook.com>
2018-11-26 20:33:57 +0000

    adding in angular js - old favourite :)

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,47 +1,13 @@
-Vue.use(VueTouch);
+angular
+    .module('app', ['ngTouch']);
 
-var example1 = new Vue({
-    el: '#example-1',
-    data: {
-      counter: 0,
-      test: 'hello from vue js!!!'
-    }
-  })
 
-function reqListener () {
-  console.log(this.responseText);
+angular
+  .module('app')
+  .controller('AppController', AppController);
+
+function AppController() {
+  var vm = this;
+
+  vm.test = 'test I have been rendered';
 }
-
-function loadXMLDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
-  xhttp.send();
-}
-
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    },
-    press: function () {
-      this.message = this.message.split('').reverse().join('')
-    },
-    mockAlert: function (event) {
-      // `this` inside methods point to the Vue instance
-      alert('Hello ' + this.name + '!')
-      // `event` is the native DOM event
-      alert(event.target.tagName)
-    }
-  }
-})
-
