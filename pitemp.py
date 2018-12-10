4c982ae30a7288c294b5f2c5bc6d3869c3db0d8d
Andy Evans <andrewevans0@outlook.com>
2018-12-10 20:08:25 +0000

    turned to float

--- pitemp.py
+++ pitemp.py
@@ -4,4 +4,4 @@ def measure_temp():
         temp = os.popen("vcgencmd measure_temp").readline()
         temp = temp.replace("'C\n", "")
         temp = temp.replace("temp=","")
-        return temp
+        return float(temp)
