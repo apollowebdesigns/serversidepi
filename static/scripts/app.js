1a17df5f0ff15cb175626136777ed63b2b05f9c1
apollowebdesigns <andrewevans0@outlook.com>
2019-09-06 18:00:46 +0100

    killing off the motors

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -84,7 +84,7 @@ $(document).ready(
                 }
             }
 
-            $('#stop').mousedown(stopRequest);
+            $('#stop').onclick(stopRequest);
             $('#forwards').mouseup(stopRequest).mousedown(() => eventSourceCreator('/forwards'));
             $('#backwards').mouseup(stopRequest).mousedown(() => eventSourceCreator('/backwards'));
             $('#right').mouseup(stopRequest).mousedown(() => eventSourceCreator('/right'));
